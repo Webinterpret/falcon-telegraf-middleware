c245ee2432d1a98542a3dd0836c36e21dee57bd1
Kamil Essekkat <kamil.e@webinterpret.com>
2018-04-19 12:21:02 +0200

    Explicit support for 3.5

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License'
